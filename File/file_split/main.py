cd510971b25b1a41f761c6dddef652811f8feb7f
azhong <zczwzw@163.com>
2021-09-27 21:33:32 +0800

    add file split

---
+++ File/file_split/main.py
@@ -0,0 +1,69 @@
+
+from pathlib import Path
+import time
+
+
+newfile = "test_{}.txt"
+file_path = "The_Great_Qin_Empire_Bigsize.txt"
+# file_path = "test_1.txt"
+
+file = Path(file_path)
+
+
+# 文件绝对路径
+print(file.absolute())
+# 文件当前路径
+print(file.cwd())
+
+
+
+# 文件状态
+print(file.stat())
+
+# 文件大小
+print(file.stat().st_size/1024/1024)
+
+split_size = 1024 * 1024 * 1024 * 6.5
+
+# 拆多少份
+split_cnt = file.stat().st_size // split_size + 1
+
+start_order = 1
+
+
+
+# 上次访问的时间
+struct_time = time.localtime(file.stat().st_atime)
+format_time = time.strftime("%Y-%m-%d %H:%M:%S", struct_time)
+print(format_time)
+
+# 最后一次修改的时间Î
+struct_time = time.localtime(file.stat().st_mtime)
+format_time = time.strftime("%Y-%m-%d %H:%M:%S", struct_time)
+print(format_time)
+
+# 最新的元数据更改的时间
+struct_time = time.localtime(file.stat().st_ctime)
+format_time = time.strftime("%Y-%m-%d %H:%M:%S", struct_time)
+print(format_time)
+
+
+def write_file(point=0, split_file=newfile.format(start_order)):
+    with open(file_path, mode="r", encoding="utf-8") as rf:
+        rf.seek(point)
+        global start_order
+        with open(split_file, mode="w", encoding="utf-8") as wf:
+            while Path(split_file).stat().st_size < split_size:
+                wf.write(rf.readline())
+        print(f"拆完第{start_order}个文件")
+        start_order += 1
+        point = rf.tell()
+        new_split_file = newfile.format(start_order)
+        return write_file(point, new_split_file)
+
+
+splitfile = newfile.format(start_order)
+print(splitfile)
+
+tell = write_file(0, splitfile)
+# print(tell)
